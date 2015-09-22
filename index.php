d168323a5bcc14d91ea5188fb41792b08ac797a1
mongotutu <w_snowing@qq.com>
2015-09-22 17:09:56 +0800

    bug

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ define('APP_PATH','./Application/');
 
 define('COMMON_PATH', dirname(dirname(__FILE__)).'/Common/'); // 应用公共目录
 $server_ip = require COMMON_PATH.'ip.php';
-define('IMAGES', $server_ip['img_server']);
+define('SERVER_IP', 'http://'.$server_ip['img_server'].'/');
 define('MONGO_SERVER', $server_ip['db_server']);
 
 require './ThinkPHP/ThinkPHP.php';
