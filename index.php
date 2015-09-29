e72d25e92f8a41ee548e1ac755a63a78626c3871
mongotutu <w_snowing@qq.com>
2015-09-29 11:24:05 +0800

    x

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 define('APP_DEBUG',true);
 define('APP_PATH','./Application/');
 
-define('COMMON_PATH', dirname(dirname(__FILE__)).'/Common/'); // 应用公共目录
-$server_ip = require COMMON_PATH.'ip.php';
+// define('COMMON_PATH', dirname(dirname(__FILE__)).'/Common/'); // 应用公共目录
+$server_ip = require dirname(dirname(__FILE__)).'/ip.php';
 define('SERVER_IP', 'http://'.$server_ip['img_server'].'/');
 define('MONGO_SERVER', $server_ip['db_server']);
 
